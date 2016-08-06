320992eeeb07f1f43beba3a75bb25691b8f1ebb8
amit4alljava <amit4alljava@gmail.com>
2016-08-06 23:42:26 +0530

    Codes

--- CollectionApps/src/Demo2.java
+++ CollectionApps/src/Demo2.java
@@ -1,19 +1,74 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Scanner;
 
+//1st Way to Use Comparator
+class SortById implements Comparator<Product>{
+	public int compare(Product one, Product two){
+		//Integer first = one.getId();
+		//Integer second = two.getId();
+		//return first.compareTo(second);
+	return ((Integer)one.getId()).compareTo((Integer)two.getId());
+	}
+}
+//class SortByName implements Comparator<Product>
+//{
+//	public int compare(Product one, Product two){
+//		//Integer first = one.getId();
+//		//Integer second = two.getId();
+//		//return first.compareTo(second);
+//	return one.getName().compareToIgnoreCase(two.getName())
+//	}
+//}
+
 // CRUD Operation
-class Product
+class Product implements Comparable<Product>
 {
 	private int id;
 	private String name;
 	private double price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	Product(int id , String name , double price){
 		this.id = id;
 		this.name = name;
 		this.price = price;
 	}
+	// Start From Java 5
+	public int compareTo(Product p){
+		//return this.name.compareTo(p.name);  // ASCII Order
+		//return this.name.compareToIgnoreCase(p.name); 
+		return p.name.compareToIgnoreCase(this.name);
+	}
+	// Before Java 5
+//	public int compareTo(Object o){
+//		Product p  = null;
+//		if(o instanceof Product){
+//		 p = (Product)o; //Downcasting
+//		 return this.name.compareTo(p.name);
+//		}
+//		return 0;
+//		
+//	}
 	@Override
 	public boolean equals(Object o){
 		Product p = (Product )o; // Downcasting
@@ -44,6 +99,37 @@ class ProductOperations{
 	private ProductOperations(){}
 	private Scanner scanner = new Scanner(System.in);
 	private ArrayList<Product> productList = new ArrayList<>();
+	public void sortById(){
+		//Collections.sort(productList); // Always Looking for Comparable
+		Collections.sort(productList,new SortById()); // Looking for Comparator
+		System.out.println("After Sort "); 
+		this.print();
+	}
+	public void sortByName(){
+		//Collections.sort(productList); // Always Looking for Comparable
+//		Comparator<Product> comp= new Comparator<Product>(){
+//			public int compare(Product one, Product two){
+//				return one.getName().compareToIgnoreCase(two.getName());
+//			}
+//		};
+		//Collections.sort(productList,comp); // Looking for Comparator
+//		Collections.sort(productList,new Comparator<Product>(){
+//			public int compare(Product one, Product two){
+//			return one.getName().compareToIgnoreCase(two.getName());
+//		}
+//	});
+		Collections.sort(productList,(one,two)->one.getName().compareTo(two.getName()));
+		System.out.println("After Sort "); 
+		this.print();
+	}
+	public void sortByPrice(){
+		//Collections.sort(productList); // Always Looking for Comparable
+		//Collections.sort(productList,new SortById()); // Looking for Comparator
+		Collections.sort(productList,(one,two)->((Double)one.getPrice()).compareTo(((Double)two.getPrice())));
+		
+		System.out.println("After Sort "); 
+		this.print();
+	}
 	public void delete(){
 		int index = searchProduct();
 		if(index>=0){
@@ -166,7 +252,9 @@ public class Demo2 {
 		case 4:
 			prodOpr.delete();
 			break;
-			
+		case 7 :
+			prodOpr.sortById();
+			break;
 		}
 		}
 
